232beb143065c9d478843a0540a38cc8a1c81883
Eros Casas <ecasas182015@gmail.com>
2021-05-06 23:15:18 -0500

    started website convertion

---
+++ src/components/TopNavbar/Topnavbar.js
@@ -0,0 +1,13 @@
+import React ,{ Component } from "react";
+import './topnavbar.css';
+
+class Topnavbar extends Component {
+    render() { 
+        return <div className="topnavbar">
+            <h1>navbar</h1>
+        </div>;
+        
+    }
+}
+
+export default Topnavbar;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Topnavbar from './components/TopNavbar/Topnavbar';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Topnavbar />
   </React.StrictMode>,
   document.getElementById('root')
 );
