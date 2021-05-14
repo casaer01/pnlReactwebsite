01129bdd928e7c9cf338818a49122208b9670e0d
Eros Casas <ecasas182015@gmail.com>
2021-05-14 13:05:08 -0500

    top navbar & slideshow

--- src/components/TopNavbar/Topnavbar.js
+++ src/components/TopNavbar/Topnavbar.js
@@ -1,11 +1,29 @@
 import React ,{ Component } from "react";
 import './topnavbar.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
 
 class Topnavbar extends Component {
     render() { 
-        return <div className="topnavbar">
-            <h1>navbar</h1>
-        </div>;
+        return <Container> 
+            <Navbar className="justify-content-end" fixed="top" bg="danger" variant="light">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="mr-auto ">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">About</Nav.Link>
+                <Nav.Link href="#pricing">Contact Us</Nav.Link>
+                <NavDropdown title="Our Product" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Menu</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.2">Tortillas</NavDropdown.Item>
+                    <NavDropdown.Item href="#action/3.3">Sweets</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Special Orders</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar>
+      </Container>;
         
     }
 }
