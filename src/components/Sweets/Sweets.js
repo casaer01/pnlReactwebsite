0a2d3c58c4e2735e55433e9c30519f19fa970efe
Eros Casas <ecasas182015@gmail.com>
2021-08-20 18:31:00 -0500

    small changes to mobile screen

--- src/components/Sweets/Sweets.js
+++ src/components/Sweets/Sweets.js
@@ -1,8 +1,8 @@
-import React, {Component} from "react";
-import {Container, Row, Col} from "react-bootstrap";
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./Sweets.css";
 
-class Sweets extends Component{
+class Sweets extends Component {
     render() {
         return (
             <Container fluid className="SweetsPage" >
@@ -10,6 +10,7 @@ class Sweets extends Component{
                     <Col>
                         <h1>· Sweets ·</h1>
                         <h3>Traditional Mexican pastries and more, all baked fresh daily</h3>
+                        <br></br>
                     </Col>
                 </Row>
                 <Row>
@@ -17,12 +18,14 @@ class Sweets extends Component{
                         <h2>· Pan Dulce ·</h2>
                         <h5>Mexican Sweet Breads</h5>
                         <p>Conchas-White, Chocolate, Yellow, Pink (vegan) Panda Muerto, Rosca Reyas, Bunuelos seasonal and by special order </p>
+                        <hr></hr>
                     </Col>
 
                     <Col>
                         <h2>· Cookies ·</h2>
                         <h5>Mexican style sugar cookies and traditional favorites</h5>
                         <p>Ojorasca (Cinnamon sugar), Butter Cookies, Payaso (sugar), Shortbread, Chocolate Chip, & more</p>
+                        <hr></hr>
                     </Col>
                 </Row>
                 <Row>
@@ -30,11 +33,13 @@ class Sweets extends Component{
                         <h2>· Mini Conchas ·</h2>
                         <h5>A smaller version of our traditional Conchas pan dulce</h5>
                         <p>White, Chocolate, Yellow, Pink (vegan) Custom colors/flavors available by order </p>
+                        <hr></hr>
                     </Col>
                     <Col>
                         <h2>· Doughnuts ·</h2>
                         <h5>Pillowy soft in a assortment of sizes and flavors</h5>
                         <p>Chocolate, sugar, glazed, vanilla, custard filled</p>
+                        <hr></hr>
                     </Col>
                 </Row>
                 <Row>
@@ -42,11 +47,13 @@ class Sweets extends Component{
                         <h2>· Churros ·</h2>
                         <h5>Delicious fried-dough pastry</h5>
                         <p>Cinnamon and sugar</p>
+                        <hr></hr>
                     </Col>
                     <Col>
                         <h2>· Flaky Puff Pastries ·</h2>
                         <h5>Mexican style Pasteries</h5>
                         <p>Apple, pineapple, cream cheese, sugar-coated </p>
+                        <hr></hr>
                     </Col>
                 </Row>
                 <Row>
@@ -54,11 +61,13 @@ class Sweets extends Component{
                         <h2>· Flan ·</h2>
                         <h5>Egg Custard cake</h5>
                         <p>Whole cakes or by-the-slice</p>
+
                     </Col>
                     <Col>
                         <h2>· Pastries ·</h2>
                         <h5>Cakes and baked sweets</h5>
                         <p>Cupcakes, confetti cakes, poundcake & more</p>
+
                     </Col>
                 </Row>
                 <Row>
@@ -66,6 +75,7 @@ class Sweets extends Component{
                         <h2>· Tres Leche ·</h2>
                         <h5>"Three Milk" Cake</h5>
                         <p>Assortment of fillings - whole cake or by-the-slice</p>
+
                     </Col>
                 </Row>
             </Container>
--- src/components/footerPNL/footerPNL.js
+++ src/components/footerPNL/footerPNL.js
@@ -1,4 +1,4 @@
-import React ,{ Component } from "react";
+import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -8,64 +8,64 @@ import instrapnl from "../../images/insta.png"
 import yelppnl from "../../images/yelpsm.png"
 import './footerPNL.css';
 
-class FooterPNL extends Component{
+class FooterPNL extends Component {
     render() {
         return <div className="footermain" >
             <Container fluid >
                 <Row className="footerRow" >
                     <Col xs={3} >
-                    <img src={pnldistlogo} alt="Pnl's logo" width="100%" />
+                        <img src={pnldistlogo} className="footerLogo" alt="Pnl's logo" width="100%" />
                     </Col>
 
                     <Col xs={1} >
                     </Col>
 
                     <Col>
-                    <dl>
-                        <dt><a className="links" href="/Menu" >Menu</a></dt>
-                        <dt><a className="links" href="/Tortillas" >Tortillas</a></dt>
-                        <dt><a className="links" href="/Sweets" >Sweets</a></dt>
-                        <dt><a className="links" href="/#aboutpnl">About</a></dt>
-                        <dt><a className="links" href="/ContactUs" >Contact Us</a></dt>
-                    </dl>
+                        <dl>
+                            <dt><a className="links" href="/Menu" >Menu</a></dt>
+                            <dt><a className="links" href="/Tortillas" >Tortillas</a></dt>
+                            <dt><a className="links" href="/Sweets" >Sweets</a></dt>
+                            <dt><a className="links" href="/#aboutpnl">About</a></dt>
+                            <dt><a className="links" href="/ContactUs" >Contact Us</a></dt>
+                        </dl>
                     </Col>
 
                     <Col>
-                    <dl>
-                        <dt>Telephone:</dt>
-                        <a className="links" href="tel:+13122435977">
-                            <dt>(312) 243-5977</dt>
-                        </a>
-                        <br />
-                        <dt>Address:</dt>
-                        <a className="links" href="https://goo.gl/maps/wVDQASLvuC4qx46E9" >
-                            <dt>1634 W 18th St</dt>
-                            <dt>Chicago, Illinois</dt>
-                        </a>
-                    </dl>
+                        <dl>
+                            <dt>Telephone:</dt>
+                            <a className="links" href="tel:+13122435977">
+                                <dt>(312) 243-5977</dt>
+                            </a>
+                            <br />
+                            <dt>Address:</dt>
+                            <a className="links" href="https://goo.gl/maps/wVDQASLvuC4qx46E9" >
+                                <dt>1634 W 18th St</dt>
+                                <dt>Chicago, Illinois</dt>
+                            </a>
+                        </dl>
                     </Col>
 
                     <Col xs={5} >
-                    <dl>
-                        <dt>Hours</dt>
-                        <dt>Mon-Fri: 6:00 am - 8:00 pm</dt>
-                        <dt>Sat Sun: 8:00 am - 8:00 pm</dt>
-                        <br />
-                        <dt>Follow Us</dt>
-                        <dt>
-                            <a href="https://www.facebook.com/PanaderiaNuevoLeon/"  >
-                                <img src={fbpnl} alt="Pnl's facebook" width="55px" />
-                            </a>
+                        <dl>
+                            <dt>Hours</dt>
+                            <dt>Mon-Fri: 6:00 am - 8:00 pm</dt>
+                            <dt>Sat Sun: 8:00 am - 8:00 pm</dt>
+                            <br />
+                            <dt>Follow Us</dt>
+                            <dt>
+                                <a href="https://www.facebook.com/PanaderiaNuevoLeon/"  >
+                                    <img src={fbpnl} alt="Pnl's facebook" width="55px" />
+                                </a>
 
-                            <a href="https://www.instagram.com/pnl_pilsen/" >
-                                <img src={instrapnl} alt="Pnl's instagram" width="55px" />
-                            </a>
-                            
-                            <a href="https://www.yelp.com/biz/nuevo-leon-bakery-chicago-3?osq=panaderia+nuevo+leon" >
-                                <img src={yelppnl} alt="Pnl's yelp" width="55px" />
-                            </a>
-                        </dt>
-                    </dl>
+                                <a href="https://www.instagram.com/pnl_pilsen/" >
+                                    <img src={instrapnl} alt="Pnl's instagram" width="55px" />
+                                </a>
+
+                                <a href="https://www.yelp.com/biz/nuevo-leon-bakery-chicago-3?osq=panaderia+nuevo+leon" >
+                                    <img src={yelppnl} alt="Pnl's yelp" width="55px" />
+                                </a>
+                            </dt>
+                        </dl>
                     </Col>
                 </Row>
             </Container>
