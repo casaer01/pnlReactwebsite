714493baf8b6450ae7a0ba874eecd5380ea3ac11
casaer01 <ecasas182015@gmail.com>
2024-06-27 13:13:25 -0500

    pushing changes of Top navbar

--- src/components/TopNavbar/Topnavbar.js
+++ src/components/TopNavbar/Topnavbar.js
@@ -5,11 +5,74 @@ import './topnavbar.css';
 class Topnavbar extends Component {
     render() {
         return (
-            <div>
+            <header>
+            {/* <!-- Navbar --> */}
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top mask-custom shadow-0">
+                <div class="container">
+                <a class="navbar-brand" href="#!"><span style={{color: '#5e9693'}}>Panaderia</span><span style={{color: '#fff'}}>Nuevo Leon</span></a>
 
-                <h1>Top Nav Bar</h1>
+                <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
 
-            </div>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                        <ul class="navbar-nav me-auto">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#!">Offer</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#!">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#!">Portfolio</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#!">Reference</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#!">About</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#!">Team</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#!">Contact</a>
+                            </li>
+                        </ul>
+
+                        <ul class="navbar-nav d-flex flex-row">
+                            <li class="nav-item me-3 me-lg-0">
+                                <a class="nav-link" href="#!">
+                                <i class="fas fa-shopping-cart"></i>
+                                </a>
+                            </li>
+                            <li class="nav-item me-3 me-lg-0">
+                                <a class="nav-link" href="#!">
+                                <i class="fab fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li class="nav-item me-3 me-lg-0">
+                                <a class="nav-link" href="#!">
+                                <i class="fab fa-instagram"></i>
+                                </a>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
+            {/* <!-- Navbar --> */}
+
+            {/* <!--Section: Design Block--> */}
+            <section>
+                {/* <!-- Intro --> */}
+                <div class="mask" style={{ backgroundcolor: 'rgba(250, 182, 162, 0.15)' }} ></div>
+
+                {/* <!-- Intro --> */}
+            </section>
+            {/* <!--Section: Design Block--> */}
+            </header>
         );
     }
 }
